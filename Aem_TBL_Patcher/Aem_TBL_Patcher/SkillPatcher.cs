c3b4501380aea969450a3386513a8a53f2f03bb9
T-Pose Ratkechi <tposeratkechi@gmail.com>
2020-10-19 21:20:33 -0500

    updated skill patcher to use list patchers

--- Aem_TBL_Patcher/Aem_TBL_Patcher/SkillPatcher.cs
+++ Aem_TBL_Patcher/Aem_TBL_Patcher/SkillPatcher.cs
@@ -10,8 +10,19 @@ namespace Aem_TBL_Patcher
         public List<PatchEdit> GetPatches(byte[] originalBytes, byte[] moddedBytes)
         {
             List<PatchEdit> thePatches = new List<PatchEdit>();
-            GetElementPatches(thePatches, originalBytes, moddedBytes);
-            GetSkillPatches(thePatches, originalBytes, moddedBytes);
+            //GetElementPatches(thePatches, originalBytes, moddedBytes);
+            //GetSkillPatches(thePatches, originalBytes, moddedBytes);
+            ListPatcher[] patchers = new ListPatcher[]
+            {
+                new ListPatcher(0, 2, "Elements"),
+                new ListPatcher(1264, 44, "Skills"),
+            };
+
+            foreach (ListPatcher patcher in patchers)
+            {
+                patcher.GenerateListPatches(thePatches, originalBytes, moddedBytes);
+            }
+
             Console.WriteLine($"SKL - Total Patches Needed: {thePatches.Count}");
 
             return thePatches;
