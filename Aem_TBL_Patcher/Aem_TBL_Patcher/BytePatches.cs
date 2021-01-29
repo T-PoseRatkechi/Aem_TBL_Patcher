3b9b78f74a91f1e4edabeae2b4d9594ed214b6aa
T-Pose Ratkechi <tposeratkechi@gmail.com>
2021-01-29 12:54:18 -0600

    Fixed BytePatches to handle creating patches for modded tbls larger than the original tbl.

--- Aem_TBL_Patcher/Aem_TBL_Patcher/BytePatches.cs
+++ Aem_TBL_Patcher/Aem_TBL_Patcher/BytePatches.cs
@@ -7,19 +7,20 @@ namespace Aem_TBL_Patcher
     class BytePatches : IPatchGenerator
     {
         string tbl = null;
-        int start = 0;
-        int end = 0;
+        int patchByteStart = 0;
+        int patchByteEnd = 0;
 
         public BytePatches(string tblName, int startByte, int endByte)
         {
             tbl = tblName;
-            start = startByte;
-            end = endByte;
+            patchByteStart = startByte;
+            patchByteEnd = endByte;
         }
 
         public void GeneratePatches(List<PatchEdit> patches, byte[] originalBytes, byte[] moddedBytes)
         {
-            for (int byteIndex = start, totalBytes = end; byteIndex < totalBytes; byteIndex++)
+            // handle creating patches upto length of originaBytes array
+            for (int byteIndex = patchByteStart; byteIndex < patchByteEnd; byteIndex++)
             {
                 byte currentOriginalByte = originalBytes[byteIndex];
                 byte currentModdedByte = moddedBytes[byteIndex];
@@ -34,21 +35,48 @@ namespace Aem_TBL_Patcher
                     };
 
                     // read ahead for the edited bytes
-                    for (int byteEditIndex = byteIndex, byteCount = 0; byteEditIndex < totalBytes; byteEditIndex++, byteCount++)
+                    for (int byteEditIndex = byteIndex, byteCount = 0; byteEditIndex < patchByteEnd; byteEditIndex++, byteCount++)
                     {
-                        // exit loop once bytes match again
-                        if (originalBytes[byteEditIndex] == moddedBytes[byteEditIndex])
+                        // handle creating patches that reach up to the length of the original tbl
+                        if (byteEditIndex == originalBytes.Length - 1)
+                        {
+                            byte[] tempData = new byte[byteCount + 1];
+                            Array.Copy(moddedBytes, byteIndex, tempData, 0, byteCount + 1);
+                            newPatch.data = PatchDataFormatter.ByteArrayToHexText(tempData);
+                            byteIndex = byteEditIndex;
+                            break;
+                        }
+                        // handle creating patches within the range of the original tbl
+                        else if (originalBytes[byteEditIndex] == moddedBytes[byteEditIndex])
                         {
                             byte[] tempData = new byte[byteCount];
                             Array.Copy(moddedBytes, byteIndex, tempData, 0, byteCount);
                             newPatch.data = PatchDataFormatter.ByteArrayToHexText(tempData);
-                            byteIndex = byteEditIndex - 1;
+                            byteIndex = byteEditIndex;
                             break;
                         }
                     }
 
                     patches.Add(newPatch);
                 }
+            }
+
+            // handle creating patches past the original length of the tbl
+            // if the modded tbl extends past that and a patch was wanted for the entire tbl
+            if (patchByteEnd >= originalBytes.Length && originalBytes.Length < moddedBytes.Length)
+            {
+                byte[] extendedBytes = moddedBytes[originalBytes.Length..moddedBytes.Length];
+
+                PatchEdit newPatch = new PatchEdit
+                {
+                    tbl = tbl,
+                    offset = originalBytes.Length,
+                    data = PatchDataFormatter.ByteArrayToHexText(extendedBytes)
+                };
+
+                Console.WriteLine("Extended Byte Patch");
+
+                patches.Add(newPatch);
             }
         }
     }
