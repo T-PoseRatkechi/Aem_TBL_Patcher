8388c7dec665cf1320803e5d2994d3c291a94a2e
T-Pose Ratkechi <tposeratkechi@gmail.com>
2021-01-30 16:16:21 -0600

    Added info display for BytePatches: number of bytes edited, total patches created, and total extended patches created.

--- Aem_TBL_Patcher/Aem_TBL_Patcher/BytePatches.cs
+++ Aem_TBL_Patcher/Aem_TBL_Patcher/BytePatches.cs
@@ -20,6 +20,11 @@ namespace Aem_TBL_Patcher
             int patchBytesStart = _segmentProps.OriginalOffset;
             int patchBytesEnd = (int)_segmentProps.OriginalSize + patchBytesStart;
 
+            // track patch info
+            int totalBytesEdited = 0;
+            int patchesCount = 0;
+            int extraPatchesCount = 0;
+
             for (int byteIndex = patchBytesStart; byteIndex < patchBytesEnd; byteIndex++)
             {
                 byte currentOriginalByte = originalBytes[byteIndex];
@@ -41,6 +46,8 @@ namespace Aem_TBL_Patcher
                         // handle creating patches that reach up to the length of the original tbl
                         if (byteEditIndex == originalBytes.Length - 1)
                         {
+                            totalBytesEdited += byteCount + 1;
+
                             byte[] tempData = new byte[byteCount + 1];
                             Array.Copy(moddedBytes, byteIndex, tempData, 0, byteCount + 1);
                             newPatch.data = PatchDataFormatter.ByteArrayToHexText(tempData);
@@ -50,6 +57,8 @@ namespace Aem_TBL_Patcher
                         // handle creating patches within the range of the original tbl
                         else if (originalBytes[byteEditIndex] == moddedBytes[byteEditIndex])
                         {
+                            totalBytesEdited += byteCount;
+
                             byte[] tempData = new byte[byteCount];
                             Array.Copy(moddedBytes, byteIndex, tempData, 0, byteCount);
                             newPatch.data = PatchDataFormatter.ByteArrayToHexText(tempData);
@@ -59,14 +68,17 @@ namespace Aem_TBL_Patcher
                     }
                     
                     patches.Add(newPatch);
+                    patchesCount++;
                 }
             }
             // add rest of expanded bytes as patch
             if (_segmentProps.ModSize > _segmentProps.OriginalSize)
             {
                 int expandedSize = (int)(_segmentProps.ModSize - _segmentProps.OriginalSize);
+
                 byte[] tempData = new byte[expandedSize];
                 Array.Copy(moddedBytes, (int)(_segmentProps.OriginalSize + _segmentProps.ModOffset), tempData, 0, expandedSize);
+
                 PatchEdit newPatch = new PatchEdit
                 {
                     tbl = tbl,
@@ -74,8 +86,12 @@ namespace Aem_TBL_Patcher
                     offset = (int)_segmentProps.OriginalSize,
                     data = PatchDataFormatter.ByteArrayToHexText(tempData)
                 };
+
+                extraPatchesCount++;
                 patches.Add(newPatch);
             }
+
+            Console.WriteLine($"[{_segmentProps.Name}] Bytes Edited: {totalBytesEdited}, Total Patches: {patchesCount + extraPatchesCount}, Extended Patches: {extraPatchesCount}");
         }
     }
 }
