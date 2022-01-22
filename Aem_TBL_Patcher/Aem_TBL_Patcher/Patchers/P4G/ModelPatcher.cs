281b93fc7b2d7e3162a45f3034bc3bde83ab6aca
T-Pose Ratkechi <tposeratkechi@gmail.com>
2022-01-22 15:24:06 -0600

    Updated segments for Persona 4 Golden MODEL.TBL.

--- Aem_TBL_Patcher/Aem_TBL_Patcher/Patchers/P4G/ModelPatcher.cs
+++ Aem_TBL_Patcher/Aem_TBL_Patcher/Patchers/P4G/ModelPatcher.cs
@@ -11,8 +11,9 @@ namespace Aem_TBL_Patcher.Patchers.P4G
 
         protected override Segment[] Segments => new Segment[]
         {
-            new Segment("ModelUnknown1"),
-            new Segment("ModelUnknown2")
+            new Segment(382, "Player Visual Variables"),
+            new Segment(232, "Enemy Visual Variables"),
+            new Segment(88, "Persona Visual Variables")
         };
     }
 }
